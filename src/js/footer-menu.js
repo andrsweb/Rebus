2390158b89ad80fbafd7d252f0b3b86720b13e45
andrsweb <proflinervk@gmail.com>
2023-08-02 09:22:39 +0700

    some fix

--- src/js/footer-menu.js
+++ src/js/footer-menu.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
-	
+
 	showMenu()
 })
 
 const showMenu = () => {
 	const buttons = document.querySelector('.footer__buttons')
 
-	if(!buttons && !menu) return
+	if (!buttons && !menu) return
 
 	buttons.addEventListener('click', e => {
 		const target = e.target
 		const prev = document.querySelector('.call-menu.clicked')
 
-		if(target.closest('.call-menu').classList.contains('clicked')) {
+		if (target.closest('.call-menu').classList.contains('clicked')) {
 			target.closest('.call-menu').classList.remove('clicked')
 			return
 		}
