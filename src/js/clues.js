ad0f1aeb9c8c4eb67430558242ebf7321dec6562
andrsweb <proflinervk@gmail.com>
2023-08-11 09:19:21 +0700

    clues adaptive

--- src/js/clues.js
+++ src/js/clues.js
@@ -6,24 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const showHiddenClues = () => {
 	const buttons = document.querySelectorAll('.last__img')
-
+	const clues   = document.querySelectorAll('.clue .clicked')
 	if (!buttons.length) return
 
 	buttons.forEach(button => {
 		const clue = button.querySelector('.clue')
 		button.addEventListener('click', () => {
-			
+
 			if (!clue) return
 			// If current clue is clicked - remove 'clicked' and exit.
 			const old = document.querySelector('.clue.clicked')
 
-			if(clue.classList.contains('clicked')) {
+			if (clue.classList.contains('clicked')) {
 				clue.classList.remove('clicked')
 				return
 			}
 
 			// If there's some other items clicked - remove clicked...
-			if(old) old.classList.remove('clicked')
+			if (old) old.classList.remove('clicked')
 
 			// ...and then enable the CURRENT one.
 			clue.classList.add('clicked')
