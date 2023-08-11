09f588e749b84d3e506537bded60032a87a3805c
andrsweb <proflinervk@gmail.com>
2023-08-11 08:09:15 +0700

    write clues logic

--- src/js/clues.js
+++ src/js/clues.js
@@ -6,18 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const showHiddenClues = () => {
 	const buttons = document.querySelectorAll('.last__img')
-	const clue = document.querySelector('.clue')
 
 	if (!buttons.length) return
 
 	buttons.forEach(button => {
 		const clue = button.querySelector('.clue')
 		button.addEventListener('click', () => {
-
+			
 			if (!clue) return
+			// If current clue is clicked - remove 'clicked' and exit.
+			const old = document.querySelector('.clue.clicked')
 
-			if (!clue.classList.contains('clicked')) clue.classList.add('clicked')
-			else clue.classList.remove('clicked')
+			if(clue.classList.contains('clicked')) {
+				clue.classList.remove('clicked')
+				return
+			}
+
+			// If there's some other items clicked - remove clicked...
+			if(old) old.classList.remove('clicked')
+
+			// ...and then enable the CURRENT one.
+			clue.classList.add('clicked')
 		})
 	})
 }
