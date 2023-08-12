fe2373c4736bc9a7809a1835cb09eb2c8b371c8d
andrsweb <proflinervk@gmail.com>
2023-08-12 15:40:16 +0700

    fix footer menu

--- src/js/footer-menu.js
+++ src/js/footer-menu.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const showMenu = () => {
 	const buttons = document.querySelector('.footer__buttons')
+	const menus   = document.querySelectorAll('.call-menu')
 
 	if (!buttons && !menu) return
 
@@ -24,4 +25,22 @@ const showMenu = () => {
 
 		target.closest('.call-menu').classList.add('clicked')
 	})
+
+	document.addEventListener( 'click', e => {  // Close footer menu by tap or click anywhere
+		e.stopPropagation()
+		target = e.target
+
+		if (
+			! target.className ||
+			target.classList.contains('footer__lang_text') ||
+			target.classList.contains('call-menu') ||
+			target.classList.contains('clicked') ||
+			target.classList.contains('footer__flag') ||
+			target.classList.contains('footer__docs_text')
+		) return
+
+		menus.forEach(menu => {
+			menu.classList.remove( 'clicked' )
+		})
+	} )
 }
